d0535131434a664f4b88025df2d838921d71bc3d
Ricky The Ising <rickytheising@gmail.com>
2024-07-27 00:10:40 +0700

    refactor main.py to use chat.py

--- src/main.py
+++ src/main.py
@@ -3,44 +3,13 @@ import os
 from dotenv import load_dotenv
 from chatbot import ChatBot, OpenAIChatBot
 from manager import AppointmentManager, CSVAppointmentManager
+from chat import generate_response
 
 def get_user_input() -> str:
     return input("You: ")
 
 def handle_exit(user_input: str) -> bool:
     return user_input.lower() == 'exit'
-
-def generate_response(chatbot: ChatBot, appointment_manager: AppointmentManager, user_input: str):
-    try:
-        details = chatbot.extract_entities_intent_dates(user_input)
-        intent = details['intention'].lower()
-        if not intent:
-            response = "Sorry, I didn't understand that. Could you state your intention (create or check) an appointment"
-        elif intent=='create':
-            exit_code, response = appointment_manager.create_appointment(
-                entity=details['entity'],
-                start=details['start_time'],
-                end=details['end_time']
-                )
-            if exit_code==2:
-                appointment_list = appointment_manager.read_appointment(
-                    date=details['date']
-                    )
-                response += f"\nHere are the list of appointments on {details['date']}\n"
-                response += appointment_list.to_string()
-        elif intent=='read':
-            appointment_list = appointment_manager.read_appointment(
-                date=details['date']
-                )
-            response = f"Here are the list of appointments on {details['date']}\n"
-            response += appointment_list.to_string()
-        else:
-            response = f"Sorry, we are currently unable to process {intent} via chatbot."
-    except Exception as e:
-        print(e)
-        response = "Sorry, I didn't understand that. Could you state your intention (create or check) an appointment"
-    
-    return response
 
 def chat(chatbot: ChatBot, appointment_manager: AppointmentManager):
     print("Welcome to the Appointment Booking Chatbot!")
